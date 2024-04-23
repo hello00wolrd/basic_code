33d36565692dd8f4b59894e7d70eb624352a6698
jianing_dai <752442633@qq.com>
2024-04-23 18:00:24 +0800

    string相关的练习

---
+++ day05/src/homeWork/LeftShiftString.java
@@ -0,0 +1,29 @@
+package homeWork;
+
+import java.util.Scanner;
+
+public class LeftShiftString {
+    public static void main(String[] args) {
+        String aStr, bStr;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string: A");
+        aStr = sc.nextLine();
+        System.out.println("Enter the string: B");
+        bStr = sc.nextLine();
+        if (aStr.length() != bStr.length()) {
+            System.out.println("A不能旋转达到B");
+            return;
+        } else {
+            for (int i = 0; i < aStr.length(); i++) {
+                if (aStr.equals(bStr)) {
+                    System.out.println("经过" + i + "次旋转,A可以变成B");
+                    return;
+                }
+                char firstChar = aStr.charAt(0);
+                String end = aStr.substring(1);
+                aStr = end + firstChar;
+            }
+            System.out.println("A不能到达B");
+        }
+    }
+}
---
+++ day05/src/homeWork/RomaNumber.java
@@ -0,0 +1,42 @@
+package homeWork;
+
+import java.util.Scanner;
+import java.util.StringJoiner;
+
+public class RomaNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str;
+        while (true) {
+            str = sc.next();
+            boolean flag = isNumber(str);
+            if (flag) {
+                break;
+            } else {
+                System.out.println("输入不合格,重新输入");
+                continue;
+            }
+        }
+        str = changeRomaNumber(str);
+        System.out.println(str);
+    }
+
+    public static String changeRomaNumber(String str) {
+        String[] changeTable = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        StringJoiner sb = new StringJoiner(" ", "[", "]");
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            sb.add(changeTable[ch - '0']);
+        }
+        return sb.toString();
+    }
+
+    public static boolean isNumber(String str) {
+        if (str.length() > 9) return false;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch < '0' || ch > '9') return false;//不是数字返回false
+        }
+        return true;
+    }
+}
